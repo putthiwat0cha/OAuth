8e2eac1dc0ae0c08d2820bace09a4f6044c6cebf
putthiwat_cha@freewillsolutions.com <putthiwat_cha@freewillsolutions.com>
2022-11-30 11:05:50 +0700

    upload

---
+++ Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DotNetOAuth.Server
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients());
+
+            if (Environment.IsDevelopment())
+                builder.AddDeveloperSigningCredential();
+
+            else
+                throw new Exception("Configuration not defined");
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
