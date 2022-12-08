18c1cabac34d581481e50dc2897480bde11986d1
putthiwat_cha@freewillsolutions.com <putthiwat_cha@freewillsolutions.com>
2022-12-08 13:47:09 +0700

    netCore

---
+++ ClientCore/Program.cs
@@ -0,0 +1,21 @@
+﻿// Net Core <5.0>
+// Microsoft.Extensions.Configuration.Abstractions <2.1.0>
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CliCredent
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostServer(args).Build()
+                            .Run();
+        }
+
+        public static IWebHostBuilder HostServer(string[] args)
+        => WebHost.CreateDefaultBuilder(args)
+                  .UseStartup<Startup>();
+    }
+}
---
+++ ClientCore/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CliCredent
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            Environment = env;
+        }
+
+        public void ConfigureServices(IServiceCollection serv)
+        {
+            var builder = serv.AddIdentityServer()
+                              .AddInMemoryIdentityResources(Config.GetIdens())
+                              .AddInMemoryApiResources(Config.GetApis())
+                              .AddInMemoryClients(Config.GetClients());
+
+            if(Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+
+            else
+            {
+                throw new Exception("Please define your configuration...");
+            }
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseIdentityServer();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
